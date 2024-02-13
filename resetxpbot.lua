c526e83d3b43a5a053622a217a32b4026aa0c5b2
HalCroves <ratatouilledu23@hotmail.fr>
2024-02-14 00:27:58 +0100

    Update resetxpbot.lua

--- resetxpbot.lua
+++ resetxpbot.lua
@@ -1,9 +1,20 @@
-function et_InitGame( levelTime, randomSeed, restart )
+local resetXPBots = false
 
-	for local i=0, tonumber(et.trap_Cvar_Get("sv_maxclients"))-1 do
+-- Fonction pour vérifier si le match est en cours
+local function isGameInProgress()
+    local gameState = et.trap_Cvar_Get("gamestate")
+    return gameState == "0" or gameState == "" or gameState == "3" -- 0 pour "GAME_STATE_PLAYING", 3 pour "GAME_STATE_POSTGAME"
+end
 
-		if et.gentity_get(i, "pers.localClient") == 1 then
-			et.trap_SendConsoleCommand(et.EXEC_APPEND, "!resetxp " .. i .. ";")
-		end
-	end
+function et_ClientBegin(clientNum)
+    if not resetXPBots and not isGameInProgress() then
+        local maxClients = tonumber(et.trap_Cvar_Get("sv_maxclients"))
+        
+        for i = 0, maxClients - 1 do
+            if tonumber(et.gentity_get(i, "pers.localClient")) == 1 then
+                et.trap_SendConsoleCommand(et.EXEC_APPEND, "!resetxp " .. i .. ";")
+            end
+        end
+        resetXPBots = true
+    end
 end
